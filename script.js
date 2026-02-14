3ef0f621db5ed320cdd20c048449ee23254835bf
DNistratov <lonsdailg@gmail.com>
2026-02-14 05:04:39 +0300

    доработали 4-й день. typeof. prompt. alert

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let userName = "Ivan";
-let userAge = 27;
-const birthYear = 1999;
+let userAge = 40;
+const birthYear = 2000;
 let isStudent = true;
+
+let name = prompt("What is your name?");
+alert("Hallo, " + userName + "! Wellcome to my site!");
+let age = prompt("How old are you? 100?");
+let ageNumber = Number(age);
+let yearsLeft = 100 - ageNumber;
+
+alert("You are" + yearsLeft + "years to 100");
